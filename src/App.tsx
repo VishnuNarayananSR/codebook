fe3d637b37e19257c7187b2b6f3a87f41411b52c
Vishnu Narayanan S R <srvishnunarayanan@gmail.com>
2023-07-08 21:38:04 +0530

    init react app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <textarea cols={100} rows={10}></textarea>
+        <br />
+        <button>Submit</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
